dd65dea70123cf0e7f402a2e7267d08882770ef7
IM-HAMZA <98418745+hmzoa@users.noreply.github.com>
2023-01-17 20:08:12 +0300

    Update instaDLv2.0.py

--- instaDLv2.0.py
+++ instaDLv2.0.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 import argparse
 import datetime
-import time
 import random
 import string
 import os
@@ -14,7 +14,6 @@ except:
     except:print("requirements.txt not found .")
 colorama.init()
 colorama.Fore.RESET
-
 sended = colorama.Fore.LIGHTWHITE_EX + " [" + colorama.Fore.LIGHTGREEN_EX + "*" + colorama.Fore.LIGHTWHITE_EX + "] "
 annowns = colorama.Fore.LIGHTWHITE_EX + " [" + colorama.Fore.LIGHTRED_EX + "!" + colorama.Fore.LIGHTWHITE_EX + "] "
 msgFromProg = colorama.Fore.LIGHTWHITE_EX + " [" + colorama.Fore.LIGHTYELLOW_EX + "~" + colorama.Fore.LIGHTWHITE_EX + "] "
@@ -28,49 +27,42 @@ logo = f"""{colorama.Fore.MAGENTA}
  |_|_| |_|___/\__\__,_|____/|_____|
                         {colorama.Fore.LIGHTBLACK_EX}by - @hmzoa{colorama.Fore.RESET}
 """
-
 class instaDL:
     def __init__(self) -> None:
+        self.versionUrl : str = "https://raw.githubusercontent.com/hmzoa/instaDL/main/Version"
+        self.currentVersion : str = "2.0.1"
+        self.currentFile : str = __file__.split("\\")[-1]
         parser = argparse.ArgumentParser(
                 description=f'{added}a script to download any media from instagram',
-                usage=f"{colorama.Fore.LIGHTBLACK_EX}python {colorama.Fore.LIGHTWHITE_EX}argpasretest.py {colorama.Fore.RESET}[-h] [-l {colorama.Fore.LIGHTCYAN_EX}<media-link>{colorama.Fore.RESET}] [-s {colorama.Fore.LIGHTGREEN_EX}<session-id>{colorama.Fore.RESET}] [-d] [{colorama.Fore.LIGHTCYAN_EX}<media-link>{colorama.Fore.RESET}]",
+                usage=f"{colorama.Fore.LIGHTBLACK_EX}python {colorama.Fore.LIGHTWHITE_EX}{self.currentFile} {colorama.Fore.RESET}[-h] [-l {colorama.Fore.LIGHTCYAN_EX}<media-link>{colorama.Fore.RESET}] [-s {colorama.Fore.LIGHTGREEN_EX}<session-id>{colorama.Fore.RESET}] [-d] [{colorama.Fore.LIGHTCYAN_EX}<media-link>{colorama.Fore.RESET}]",
                 epilog=f"{colorama.Fore.LIGHTRED_EX}───────────────────────────────────────┘{colorama.Fore.YELLOW}⌠{colorama.Fore.LIGHTBLACK_EX}by @hmzoa{colorama.Fore.YELLOW}⌡{colorama.Fore.LIGHTRED_EX}┌───────────────────────────────────────"
             )
-
         parser.add_argument('link',nargs='?',metavar='<media-link>',type=str,help=f'{colorama.Fore.GREEN}the post url link{colorama.Fore.RESET}',default="empty")
         parser.add_argument('-l','--link',dest='link_flag',type=str,metavar='<media-link>',help='the post url link',default="empty")
-
         parser.add_argument('-s','--sessionid',type=str,metavar='<session-id>',help='sessionid of an account that follows the private profile ',default="empty")
-
         debugMode = parser.add_mutually_exclusive_group()
         debugMode.add_argument('-d','--debug',action='store_true',help="turn on debug mode to print requests responses")
-
         self.args = parser.parse_args()
-
-        self.cookies = requests.get("https://instagram.com/").cookies
-
         self.url : str = self.args.link if self.args.link != "empty" else self.args.link_flag        
         self.sessionid : str = self.args.sessionid 
         self.pk : str = ""
-        
         self.mediaName : str = ""
         self.mediaNameList : list = []
         print(logo)
-
         if len(sys.argv)==1:
             parser.print_help()
             exit()
-            sys.exit()
-        elif self.sessionid == "empty" : # and self.username == "empty" and self.password == "empty":
+        elif self.sessionid == "empty" :
             self.publicMediaDL()
         elif self.sessionid != "empty" :
             self.privateMediaDL()
         else:
-            self.log(self.sessionid)      
-
-    def log(self,text):
-        print(text)
-
+            ""#working on it : login with user and password to download private post python instaDL.py https://intsgram.com/examplePost -u username -p    
+    def __del__(self):
+        getVersion = requests.get(self.versionUrl).text
+        if getVersion != self.currentVersion:
+            print(f"\n{colorama.Fore.LIGHTRED_EX}  -  you are running older version v{self.currentVersion}")
+            print(f"{colorama.Fore.LIGHTGREEN_EX}  -  version v{getVersion.strip('/n')[0]} is available on https://github.com/hmzoa/instaDL/")
     def publicMediaDL(self):
         print(f"{msgFromProg}Conforming URL.")
         if self.conformUrl():
@@ -85,7 +77,6 @@ class instaDL:
             print(f"{annowns}Invalid URL {colorama.Fore.LIGHTYELLOW_EX + str(self.url) } .")
             exit()
     def privateMediaDL(self):
-
             print(f"{msgFromProg}Conforming URL.")
             if self.conformUrl():
                 print(f"{sended}Conformed successfully.")
@@ -107,7 +98,6 @@ class instaDL:
                     if self.url[-1] != "/":
                         self.url = self.url + "/"
                         if self.args.debug:print(self.url)
-
                     self.url = self.url + "?__a=1&__d=dis"
                     if self.args.debug:print(self.url)
                 return True
@@ -125,7 +115,6 @@ class instaDL:
                     'cookie':'ig_did=7E1A5C43-633C-4514-9181-070791BD757D; ds_user_id=25025320',#22187948569
                     "X-CSRFtoken": "ZhhR4RZU5U9qxG8cOXZ8kj1Oq8fB5aH9"
                 },
-                
             )
             if self.args.debug:print(getDLurl.text)
             try :
@@ -159,45 +148,36 @@ class instaDL:
                     f.write(downloadReq.content)
                     f.flush()
                 return True
-
             elif jsonLoaded["graphql"]["shortcode_media"]["__typename"] == "GraphSidecar":
+                for item in jsonLoaded["graphql"]["shortcode_media"]["edge_sidecar_to_children"]["edges"]:
+                    try :
+                        if item["node"]["__typename"] == "GraphImage":
+                            self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} - {str(datetime.datetime.now()).split(".")[0].replace(":","-")} - {str(item["node"]["display_resources"][-1]["src"]).split("?")[0].split("/")[-1]}'#f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} _ {str(datetime.datetime.now()).split(".")[0].replace(":","-")}.jpg'
+                            DLurl = item["node"]["display_resources"][-1]["src"]
+                            if self.args.debug:print(DLurl)
+                            downloadReq = requests.get(DLurl ,stream=True )
+                            if self.args.debug:print(downloadReq)
+                            try : os.mkdir("instaPhotos")
+                            except FileExistsError : pass
+                            with open(f'instaPhotos/{self.mediaName}','wb') as f:
+                                f.write(downloadReq.content)
+                                f.flush()
+                        elif item["node"]["__typename"] == "GraphVideo":
+                            self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} - {str(datetime.datetime.now()).split(".")[0].replace(":","-")} - {str(item["node"]["video_url"]).split("?")[0].split("/")[-1]}'#self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} _ {str(datetime.datetime.now()).split(".")[0].replace(":","-")}.mp4'
+                            DLurl = item["node"]["video_url"]
+                            if self.args.debug:print(DLurl)
+                            downloadReq = requests.get(DLurl ,stream=True )
+                            if self.args.debug:print(downloadReq)
+                            try : os.mkdir("instaVideos")
+                            except FileExistsError : pass
+                            with open(f'instaVideos/{self.mediaName}','wb') as f:
+                                f.write(downloadReq.content)
+                                f.flush()
+                        print(f'  -  {colorama.Fore.LIGHTBLACK_EX}Media done {colorama.Fore.LIGHTGREEN_EX}√{colorama.Fore.RESET}')
+                    except:return False 
+                self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} -GraphSidecar'
+                return True
                 
-                try:
-                    for item in jsonLoaded["graphql"]["shortcode_media"]["edge_sidecar_to_children"]["edges"]:
-                        try :
-                            if item["node"]["__typename"] == "GraphImage":
-                                self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} _ {str(datetime.datetime.now()).split(".")[0].replace(":","-")}.jpg'
-                                try:
-                                    DLurl = item["node"]["display_resources"][-1]["src"]
-                                    if self.args.debug:print(DLurl)
-                                except KeyError:return False
-                                downloadReq = requests.get(DLurl ,stream=True )
-                                if self.args.debug:print(downloadReq)
-                                try : os.mkdir("instaPhotos")
-                                except FileExistsError : pass
-                                with open(f'instaPhotos/{self.mediaName}','wb') as f:
-                                    f.write(downloadReq.content)
-                                    f.flush()
-                                time.sleep(1)
-                            elif item["node"]["__typename"] == "GraphVideo":
-                                self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} _ {str(datetime.datetime.now()).split(".")[0].replace(":","-")}.mp4'
-                                try:
-                                    DLurl = item["node"]["video_url"]
-                                    if self.args.debug:print(DLurl)
-                                except KeyError:return False
-                                downloadReq = requests.get(DLurl ,stream=True )
-                                if self.args.debug:print(downloadReq)
-                                try : os.mkdir("instaVideos")
-                                except FileExistsError : pass
-                                with open(f'instaVideos/{self.mediaName}','wb') as f:
-                                    f.write(downloadReq.content)
-                                    f.flush()
-                                time.sleep(1)
-                            print(f'  -  {colorama.Fore.LIGHTBLACK_EX}Media done {colorama.Fore.LIGHTGREEN_EX}√{colorama.Fore.RESET}')
-                        except:return False 
-                    self.mediaName = f'{jsonLoaded["graphql"]["shortcode_media"]["owner"]["username"]} -GraphSidecar'
-                    return True
-                except : return False
             else : return False
         elif self.sessionid != "empty":
             try:self.pk = self.sessionid.split('%')[0]
@@ -250,4 +230,4 @@ class instaDL:
                 if self.args.debug:print("non of options")
                 return False
 if __name__ == '__main__':
-    instaDL()
+    app = instaDL()
